b3848168d9b173efa89f5c157da6e50bbca22fb2
Mateus Brancō <137534750+MMBranco@users.noreply.github.com>
2026-03-10 22:16:21 -0300

    ALGORITMOS 10/03/26
    Algoritmo que soma os pares entre dois intervalos definidos por um usuário. Ex: Soma todos os pares entre 1 e 10.

---
+++ somaPares.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class somaPares {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int soma = 0;
+
+        for (int i = a; i <= b; i++) {
+            if (i % 2 == 0) {
+                soma += i;
+            }
+        }
+
+        System.out.println(soma);
+        
+        sc.close();
+    }
+}
